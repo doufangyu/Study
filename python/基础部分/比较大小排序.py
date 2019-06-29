7bb6554c5fe3cbdb6cfecfdb488025a75228dd0a
chaiio <doufangyu@163.com>
2019-06-29 22:26:28 +0800

    init

---
+++ python/基础部分/比较大小排序.py
@@ -0,0 +1,11 @@
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+if a > b:
+    print(a,b)
+else:
+    if a < b:
+        print(b,a)
+    else:
+        print(a,b)
---
+++ python/基础部分/计算求平均.py
@@ -0,0 +1,13 @@
+
+
+count = 0
+Str = int(input("input :"))
+
+sum = 0
+
+for n in range(Str):
+    count += 1
+    sum = 1
+
+    av = sum / count
+    print(av)
